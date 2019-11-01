ac59f656b1d7c4ed204ae83587ed8aef13f95402
Doan Hong Ngoc <ngocdh236@gmail.com>
2019-11-01 19:38:36 +0200

    Removed database connection

--- server.js
+++ server.js
@@ -1,64 +1,39 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
 const nodemailer = require('nodemailer');
 
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
-
-const Schema = mongoose.Schema;
-const TrafficSchema = new Schema({
-  web: String,
-  visits: Number
-});
-const Traffic = mongoose.model('traffics', TrafficSchema);
-
-const addTraffic = (req, res) => {
-  const { id } = req.params;
-  Traffic.findById(id)
-    .then(query => {
-      if (query) {
-        const { web, visits } = query;
-        query.visits = visits + 1;
-        query
-          .save()
-          .then(async () => {
-            let testAccount = await nodemailer.createTestAccount();
-            let transporter = nodemailer.createTransport({
-              service: 'Gmail',
-              auth: {
-                user: 'nhimbeobeo@gmail.com',
-                type: 'OAuth2',
-                clientId: process.env.GMAIL_CLIENT_ID,
-                clientSecret: process.env.GMAIL_CLIENT_SECRET,
-                accessToken: process.env.GMAIL_ACCESS_TOKEN,
-                refreshToken: process.env.GMAIL_REFRESH_TOKEN
-              }
-            });
-
-            await transporter.sendMail({
-              from: '"Nhim" <nhimbeobeo@gmail.com>',
-              to: 'ngocdh236@gmail.com',
-              subject: 'Traffic Tracking',
-              text: `Some one visited ${web}`
-            });
-
-            return res.status(200).json({ success: true });
-          })
-          .catch(err => res.status(400).json(err));
+const notifyTraffic = async (req, res) => {
+  const { name } = req.params;
+  try {
+    let transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        type: 'OAuth2',
+        clientId: process.env.GMAIL_CLIENT_ID,
+        clientSecret: process.env.GMAIL_CLIENT_SECRET
       }
-    })
-    .catch(err => res.status(400).json(err));
+    });
+
+    await transporter.sendMail({
+      from: '"Nhim" <nhimbeobeo@gmail.com>',
+      auth: {
+        user: 'nhimbeobeo@gmail.com',
+        accessToken: process.env.GMAIL_ACCESS_TOKEN,
+        refreshToken: process.env.GMAIL_REFRESH_TOKEN
+      },
+      to: 'ngocdh236@gmail.com',
+      subject: 'Traffic Tracking',
+      text: `Some one visited ${name}`
+    });
+
+    return res.status(200).json({ success: true });
+  } catch (err) {
+    return res.status(400).json(err);
+  }
 };
 
 const router = express.Router();
-router.post('/:id', addTraffic);
+router.post('/:name', notifyTraffic);
 
 const app = express();
 app.use(function(req, res, next) {
